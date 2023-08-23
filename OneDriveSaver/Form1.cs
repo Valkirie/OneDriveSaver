47f388b30275d9d7eef783131864cca9ab03e933
Lesueur Benjamin <benjamin.lesueur@live.com>
2023-08-23 08:56:13 +0200

    fix OneDrive detection

--- OneDriveSaver/Form1.cs
+++ OneDriveSaver/Form1.cs
@@ -46,8 +46,7 @@ namespace OneDriveSaver
                     foreach(IntPtr WindowHandle in processWindows)
                     {
                         string WindowTitle = WindowHelper.GetWindowTitle(WindowHandle);
-                        if (WindowTitle.Equals("OneDrive - Personal", StringComparison.InvariantCultureIgnoreCase)
-                            || processes.Count() == 1)
+                        if (WindowTitle.Equals("Microsoft OneDrive", StringComparison.InvariantCultureIgnoreCase) || processes.Count() == 1)
                         {
                             // store handle
                             OneDriveWindowHandle = WindowHandle;
